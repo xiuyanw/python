d9a0f01847c5ea461190a11d26f234cd5ae965e5
wangxiuyan <1273405387@qq.com>
2018-07-16 17:22:14 +0800

    my firest python

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("hello world!")
+if 3 > 0:
+    print("ok")
+    print('yes')
+print("wangxiuyan")
+if 4>5:
+    print("you are wrong")
+if 5>4:
+    print('this is right')
+x=3;y=4
+print(x+y)
+print(x*y,sep='',end='')
+print('haha')
+
---
+++ output.py
@@ -0,0 +1,15 @@
+print('hello world!!')
+print('hello','world!')
+print('hello'+'world!')
+print('hello','world!',sep='***')
+print('#'* 50)  #打印50个#
+print('how are you?',end='')
+print('你好！'*10)
+x=50
+y=2
+print(x/y)
+print(x*y)
+z=12
+print(x%z)
+print('hello','world',sep='#$%')
+print('5'*3);print('hihi')
